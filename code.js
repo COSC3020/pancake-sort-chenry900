c0c049b5ad698c36ba89ab9f99b0a7be27f27c3f
chenry900 <158115154+chenry900@users.noreply.github.com>
2024-11-11 18:50:54 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,29 @@
 function flip(array, n) {
+    var change[n]
+    var c = n - 1;
+    for (i = 0; i < n; i++)
+        change[c] = array[i];
+        c = c - 1;
+    for (i = 0; i < n; i++)
+        array[i] = change[i];
     return array;
 }
 
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
+    var largest = array[0], smallest = array[0], size = array.length();
+    var swap = 0;
+    for(i = 0; i < array.length(); i++)
+        if(largest < array[i])
+            largest = array[i];
+        if(smallest > array[i])
+            smallest = array[i];
+    while(largest != smallest)
+        for(i = 0; i < size; i++)
+            if(largest < array[i])
+                largest = array[i];
+                swap = i;
+        flip(array, (swap + 1));
+        size = size-1;
     return array;
 }
